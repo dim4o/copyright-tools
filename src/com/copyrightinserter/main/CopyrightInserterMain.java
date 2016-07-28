d3fafd6bfa2b4b95af63942d0cd626d59be30316
dim4o <d_i_m@abv.bg>
2016-07-29 00:06:49 +0300

    Add functionality for getting notice from file

--- src/com/copyrightinserter/main/CopyrightInserterMain.java
+++ src/com/copyrightinserter/main/CopyrightInserterMain.java
@@ -1,6 +1,9 @@
 package com.copyrightinserter.main;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,8 +17,16 @@ import com.copyrightinserter.io.writer.Writer;
 
 public class CopyrightInserterMain {
 
+	private static String LINE_SEPARATOR = System.getProperty("line.separator");
+
+	private static String EMPTY_STRING = "";
+	
 	public static void main(String[] args) throws IOException {
 		File root = new File("C:\\Users\\Dimcho\\Desktop\\testCopyright");
+		File noticeFile = new File("C:\\Users\\Dimcho\\Desktop\\Notice.txt");
+		
+		String notice = getNoticeFromFile(noticeFile); // "// This is top comment"
+		
 		Reader reader = new NoticeReader();
 		Writer writer = new NoticeWriter();
 		List<String> extensions = new ArrayList<String>();
@@ -23,6 +34,20 @@ public class CopyrightInserterMain {
 		extensions.add(".txt");
 		
 		Inserter inserter = new Inserter(reader, writer, extensions);
-		inserter.insert(root, "// This is top comment", NoticePosition.Top);
+		inserter.insert(root, notice, NoticePosition.Top);
+	}
+	
+	public static String getNoticeFromFile(File file) throws IOException{
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		
+		StringBuilder noticeMessage = new StringBuilder();
+		String line = EMPTY_STRING;
+		
+		while ((line = reader.readLine()) != null) {
+			noticeMessage.append(line);
+			noticeMessage.append(LINE_SEPARATOR);
+		}
+		
+		return noticeMessage.toString().trim();
 	}
 }
